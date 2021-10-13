d22ce2e7ed512cd66bb7ece5670c08a389f226ae
Aleksandr Figilyantov <a.figilyantov@gmail.com>
2021-10-13 16:37:42 +0500

    Last commit

--- University/ViewModels/GroupDTO.cs
+++ University/ViewModels/GroupDTO.cs
@@ -1,16 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace University.ViewModels
 {
     public class GroupDto
     {
         public int Id { get; set; }
-    
+
         [Display(Name = "ГРУППА")]
         public string Name { get; set; }
 
